5850873c81f3435eee8c70e7dcdf7a8829d5b0d5
pthakur3320 <pthakur3320@gmail.com>
2025-09-10 22:29:20 +0530

    udpaetd datalyer

--- js/digitalData.js
+++ js/digitalData.js
@@ -1,38 +1,30 @@
-
 window.dataLayer = window.dataLayer || [];
 
 function getPageType(pathname) {
-  if (pathname === "/githubEcom/" || pathname === "/githubEcom/index.html") {
-    return "home";
-  } else if (pathname.indexOf("shop.html") !== -1) {
-    return "plp";
-  } else if (pathname.indexOf("single-product.html") !== -1) {
-    return "pdp";
-  } else if (pathname.indexOf("cart.html") !== -1) {
-    return "cart";
-  } else if (pathname.indexOf("blog.html") !== -1) {
-    return "articles";
-  } else if (pathname.indexOf("contact.html") !== -1) {
-    return "contact";
-  } else if (pathname.indexOf("account.html") !== -1) {
-    return "account";
-  } else {
-    return "other";
-  }
+  if (pathname === "/githubEcom/" || pathname === "/githubEcom/index.html") return "home";
+  if (pathname.indexOf("shop.html") !== -1) return "plp";
+  if (pathname.indexOf("single-product.html") !== -1) return "pdp";
+  if (pathname.indexOf("cart.html") !== -1) return "cart";
+  if (pathname.indexOf("blog.html") !== -1) return "articles";
+  if (pathname.indexOf("contact.html") !== -1) return "contact";
+  if (pathname.indexOf("account.html") !== -1) return "account";
+  return "other";
 }
 
-// Push page attributes to GTM dataLayer
 document.addEventListener("DOMContentLoaded", function () {
   var path = window.location.pathname || "/";
   var pageType = getPageType(path);
   var pageName = document.title || pageType || "page";
 
-  dataLayer.push({
-    event: "page_view",
-    page: {
-      pageName: pageName,
-      pageType: pageType,
-      language: document.documentElement.lang || "en"
-    }
-  });
+  // Push on next tick to separate from historyChange in Preview
+  setTimeout(function () {
+    window.dataLayer.push({
+      event: "page_view",
+      page: {
+        pageName: pageName,
+        pageType: pageType,
+        language: document.documentElement.lang || "en"
+      }
+    });
+  }, 50); // 50ms delay is enough
 });
